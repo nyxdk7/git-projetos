b0ffa21b08920e69b06485f0736099a1ac011892
Joao <joaopedro2007.pessoal@gmail.com>
2026-04-10 21:23:14 -0500

    BANCO 100

--- dash-sistem/criar_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from sqlalchemy import text
-
-with app.app_context():
-    result = db.session.execute(text("SELECT * FROM usuario;"))
-    print("USUARIOS NO BANCO:", result.fetchall())
