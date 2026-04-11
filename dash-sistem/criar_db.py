bba893bb851612796a93e57db51841857291018e
Joao <joaopedro2007.pessoal@gmail.com>
2026-04-10 21:09:24 -0500

    OK

--- dash-sistem/criar_db.py
+++ dash-sistem/criar_db.py
@@ -2,5 +2,5 @@ from app import app, db
 from sqlalchemy import text
 
 with app.app_context():
-    result = db.session.execute(text("SELECT current_database();"))
-    print("BANCO:", result.fetchone())
+    result = db.session.execute(text("SELECT * FROM usuario;"))
+    print("USUARIOS NO BANCO:", result.fetchall())
